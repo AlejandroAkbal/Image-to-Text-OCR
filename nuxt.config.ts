d268f96dca5c9c8f12cf9ad60900a97eaadb08df
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2022-10-13 15:33:40 +0000

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+export default defineNuxtConfig({
+  modules: [
+    '@vueuse/nuxt',
+    '@unocss/nuxt',
+    '@pinia/nuxt',
+    '@nuxtjs/color-mode',
+  ],
+  experimental: {
+    reactivityTransform: true,
+  },
+  unocss: {
+    preflight: true,
+  },
+  colorMode: {
+    classSuffix: '',
+  },
+})
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <Logos mb-6 />
+    <Suspense>
+      <PageView />
+      <template #fallback>
+        <div op50 italic>
+          <span animate-pulse>Loading...</span>
+        </div>
+      </template>
+    </Suspense>
+    <InputEntry />
+  </div>
+</template>
