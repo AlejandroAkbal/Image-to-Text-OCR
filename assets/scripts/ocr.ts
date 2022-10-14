9446f9c6845d4f98509e043b0c71cca882dc92aa
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2022-10-14 12:04:12 +0200

    feat: extract into module

---
+++ assets/scripts/ocr.ts
@@ -0,0 +1,21 @@
+import type Tesseract from 'tesseract.js'
+// import { OEM, PSM } from 'tesseract.js'
+
+export async function OcrRecognize(worker: Tesseract.Worker, media: Tesseract.ImageLike) {
+  await worker.load()
+
+  await worker.loadLanguage('eng')
+
+  await worker.initialize('eng')
+
+  // await worker.setParameters({
+  //   tessedit_ocr_engine_mode: OEM.LSTM_ONLY,
+  //   tessedit_pageseg_mode: PSM.AUTO,
+  // })
+
+  const { data: { text } } = await worker.recognize(media)
+
+  await worker.terminate()
+
+  return text
+}
