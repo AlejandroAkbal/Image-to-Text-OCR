c940d65c64b1095a818c2d3452e7995de43b6b93
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2022-10-14 17:11:54 +0200

    feat: improve footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,11 +4,20 @@
       text="xl gray4"
       class="m-5 flex justify-center gap-3"
     >
-      <a i-carbon-logo-github href="https://github.com/AlejandroAkbal" target="_blank" aria-hidden="true" />
+      <a
+        class="i-carbon-logo-github w-8 h-8"
+        focus="outline-none ring-2 ring-indigo-500"
+        hover="text-white"
+        href="https://github.com/AlejandroAkbal/Image-to-Text-OCR"
+        target="_blank"
+        aria-hidden="true"
+      />
     </div>
 
     <h5 class="text-sm text-gray-400 text-center">
       Privacy friendly; everything is performed on your device, no data is sent to any server.
+      <br>
+      Created by <a href="https://akbal.dev" target="_blank" class="underline">Alejandro Akbal</a>.
     </h5>
   </footer>
 </template>
