4b73c25979495b5c352d6b38b94233a95bc318f5
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2022-11-01 13:56:43 +0100

    feat(Footer): add ProductHunt badge

--- components/Footer.vue
+++ components/Footer.vue
@@ -22,6 +22,15 @@
       />
     </div>
 
+    <div class="flex justify-center mb-4">
+      <a href="https://www.producthunt.com/posts/image-to-text-2?utm_source=badge-featured&utm_medium=badge" class="text-center" target="_blank">
+        <img
+          src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=364590&theme=dark" class="w-auto h-12 filter contrast-150"
+          alt="Image&#0032;to&#0032;Text - Extract&#0032;text&#0032;from&#0032;any&#0032;image&#0032;using&#0032;OCR | Product Hunt"
+        >
+      </a>
+    </div>
+
     <h5 class="text-sm text-gray-400 text-center">
       Privacy friendly; everything is performed on your device, no data is sent to any server.
       <br>
